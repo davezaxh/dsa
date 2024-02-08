76cb4eedb979b7dcd59616481016338025104fb5
neutron <davezaxh>
2024-02-08 12:25:55 +0530

    implemented pass print logic in selection sort

--- lab/selectionsorting.c
+++ lab/selectionsorting.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 void main(){
-    int a[10], i, j, n, temp;
+    int a[10], i, j, k, n, temp;
 
     printf("Enter the number of elements: ");
     scanf("%d", &n);
@@ -19,16 +19,22 @@ void main(){
     }
 
     for(i = 0; i < n; i++){
+
+        printf("\nPass %d: ", (i+1));
         for(j = i+1; j < n; j++){
             if(a[i] > a[j]){
                 temp = a[i];
                 a[i] = a[j];
                 a[j] = temp;
             }
-        }   
+        }  
+
+        for(int k = 0; k < n; k++){
+            printf("%d ", a[k]);
+        }
     }
 
-    printf("\nAfter Sorting: ");
+    printf("\n\nAfter Sorting: ");
     for(i = 0; i < n; i++){
         printf("%d ", a[i]);
     }

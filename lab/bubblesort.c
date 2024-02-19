4be4c9ffaa63ab35e372b428f6d68f8b04d0888d
Dave Zachariah <83908831+davezaxh@users.noreply.github.com>
2024-02-19 18:55:51 +0530

    corrected logic error in bubble sort

--- lab/bubblesort.c
+++ lab/bubblesort.c
@@ -15,12 +15,12 @@ void main(){
     }
 
     printf("\nBefore Sorting: ");
-    for(int i = 0; i < n; i++){
+    for(int i = 0; i < n - 1; i++){
         printf("%d ", arr[i]);
     }
 
     for(i = 0; i < n; i++){
-        for(j = 0; j < n-1; j++){
+        for(j = 0; j < n-i-1; j++){
             if(arr[j] > arr[j+1]){
                 temp = arr[j];
                 arr[j] = arr[j+1];
